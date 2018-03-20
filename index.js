4a35b25d9db4ef795c8fbbbff447ce04581de9de
Hunter Hodnett <hunter.hodnett@reddit.com>
2018-03-20 11:48:34 -0700

    Print average to console and add newline

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const browserPerf = require('hunterchristian-browser-perf');
 const times = require('lodash/times');
 const readFile = require('./helpers/readFile.js');
-const getStandardDeviation = require('./helpers/getStandardDeviation.js');
 const writeFile = require('./helpers/writeFile.js');
 
 const options = require('minimist')(process.argv.slice(2));
@@ -29,10 +28,34 @@ const FAST_SCROLL_SPEED = 6000;
 const MODERATE_SCROLL_SPEED = 3000;
 const SLOW_SCROLL_SPEED = 800;
 // The number of times that we scroll the page, then pause
-const NUM_PAGE_SCROLLS = 30;
+const NUM_PAGE_SCROLLS = 1;
 // Scroll distance is actually defined in chrome_scroll.js
 //const SCROLL_DISTANCE = 25000;
-const NUM_SAMPLES = 10;
+const NUM_SAMPLES = 8;
+
+const average = data => {
+  var sum = data.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+
+  var avg = sum / data.length;
+  return avg;
+}
+
+const getStandardDeviation = values => {
+  var avg = average(values);
+
+  var squareDiffs = values.map(function(value){
+    var diff = value - avg;
+    var sqrDiff = diff * diff;
+    return sqrDiff;
+  });
+
+  var avgSquareDiff = average(squareDiffs);
+
+  var stdDev = Math.sqrt(avgSquareDiff);
+  return stdDev;
+};
 
 const getElementByCss = (browser, selector) =>
 	browser.waitForElementByCss(
@@ -56,33 +79,35 @@ const runPerfTest = scrollSpeed => new Promise(async (resolve, reject) => {
 		selenium: DEFAULT_SELENIUM_SERVER_URL,
 		browsers: [
 			{
-			browserName: 'chrome',
-			chromeOptions: {
-				args: ['--user-data-dir=/Users/hunter.hodnett/Library/Application Support/Google/Chrome/Default'],
-			}
+				browserName: 'chrome',
+				chromeOptions: {
+					args: ['--user-data-dir=/Users/hunter.hodnett/Library/Application Support/Google/Chrome/Default'],
+				}
 			}
 		],
 		// preScript: async browser => {
-		// 	try {
-		// 		await browser.get(URL_BEING_TESTED);
-		// 		const loginLink = await getElementByCss(browser, 'a.desktop-onboarding-sign-up__form-toggler');
-		// 		if (loginLink) {
-		// 			// TODO: detect when we have successfully logged into Okta rather
-		// 			// than sleeping for 90 seconds
-		// 			console.log('login detected');
-		// 			await loginLink.click();
-		// 			const username = await getElementByCss(browser, '#user_login');
-		// 			await username.type(options.username);
+		// 	// try {
+		// 	// 	await browser.get(URL_BEING_TESTED);
+		// 	// 	const loginLink = await getElementByCss(browser, 'a.desktop-onboarding-sign-up__form-toggler');
+		// 	// 	if (loginLink) {
+		// 	// 		// TODO: detect when we have successfully logged into Okta rather
+		// 	// 		// than sleeping for 90 seconds
+		// 	// 		console.log('login detected');
+		// 	// 		await loginLink.click();
+		// 	// 		const username = await getElementByCss(browser, '#user_login');
+		// 	// 		await username.type(options.username);
 
-		// 			const password = await getElementByCss(browser, '#passwd_login');
-		// 			await password.type(options.password);
+		// 	// 		const password = await getElementByCss(browser, '#passwd_login');
+		// 	// 		await password.type(options.password);
 
-		// 			// Calling el.click() throws an error saying that the element is no longer in the page...
-		// 			await browser.eval(`$('button[type="submit"]').click()`);
-		// 		}
-		// 	} catch (err) {
-		// 		console.error(err);
-		// 	}
+		// 	// 		// Calling el.click() throws an error saying that the element is no longer in the page...
+		// 	// 		await browser.eval(`$('button[type="submit"]').click()`);
+		// 	// 	}
+		// 	// } catch (err) {
+		// 	// 	console.error(err);
+		// 	// }
+		// 	await browser.get(URL_BEING_TESTED);
+		// 	await browser.sleep(35000);
 		// },
 		actions: [
 			...times(NUM_PAGE_SCROLLS, () => browserPerf.actions.scroll({ speed: scrollSpeed })),
@@ -99,17 +124,20 @@ const collectPerfData = async () => {
 	let fastScrollFps = await runPerfTest(FAST_SCROLL_SPEED);
 	console.log(`fast scroll FPS: ${ fastScrollFps }`);
 	samples.fastScrollFps.push(fastScrollFps);
-	console.log(`standard deviation for fast scroll: ${ getStandardDeviation(samples.fastScrollFps) }fps`);
+	console.log(`average for fast scroll: ${ average(samples.fastScrollFps) } fps`);
+	console.log(`standard deviation for fast scroll: ${ getStandardDeviation(samples.fastScrollFps) } fps`);
 
 	let moderateScrollFps = await runPerfTest(MODERATE_SCROLL_SPEED);
 	console.log(`moderate scroll FPS: ${ moderateScrollFps }`);
 	samples.moderateScrollFps.push(moderateScrollFps);
-	console.log(`standard deviation for moderate scroll: ${ getStandardDeviation(samples.moderateScrollFps) }fps`);
+	console.log(`average for moderate scroll: ${ average(samples.moderateScrollFps) } fps`);
+	console.log(`standard deviation for moderate scroll: ${ getStandardDeviation(samples.moderateScrollFps) } fps`);
 
 	let slowScrollFps = await runPerfTest(SLOW_SCROLL_SPEED);
 	console.log(`slow scroll FPS: ${ slowScrollFps }`);
 	samples.slowScrollFps.push(slowScrollFps);
-	console.log(`standard deviation for slow scroll: ${ getStandardDeviation(samples.slowScrollFps) }fps`);
+	console.log(`average for slow scroll: ${ average(samples.slowScrollFps) } fps`);
+	console.log(`standard deviation for slow scroll: ${ getStandardDeviation(samples.slowScrollFps) } fps\n`);
 }
 
 const main = async () => {
